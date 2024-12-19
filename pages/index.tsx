27e507fe530278dd252c21dbd91253bfd73b3db5
mitchell <mitchell.rian.smith@gmail.com>
2024-12-18 20:32:18 -0800

    add comment to what the pages are (very basic, just learning)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+// index page that can be accessed by url (localhost:3000/index)
+
 export default function Home() {
   return (
     <h1>hello world</h1>
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,4 +1,4 @@
-
+// test page that can be accessed by url (localhost:3000/test)
 export default function Test() {
   return (
     <h1>hello test world</h1>
